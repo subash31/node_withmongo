1710fb8d3437ed11dc32b32890128b8c8d1eb9f8
LENOVO\ELCOT <santhoshsubash352@gmail.com>
2020-11-22 12:50:29 +0530

    Testing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
+ 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
